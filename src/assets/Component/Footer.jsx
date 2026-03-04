e3d87e80893ef6d86f96d106c09b1d37c9397a4a
Ankitdeshpande26 <121634743+Ankitdeshpande26@users.noreply.github.com>
2026-03-04 17:36:44 +0530

    /logo.png

--- src/assets/Component/Footer.jsx
+++ src/assets/Component/Footer.jsx
@@ -23,7 +23,7 @@ export default function Footer() {
           <div className="col-span-1 sm:col-span-3 lg:col-span-2 text-center sm:text-left">
             <div className="flex items-center justify-center sm:justify-start space-x-2 mb-3 sm:mb-4">
               <div className="rounded-lg">
-               <img src="img/logo.png" alt=""  className="w-6 h-6 sm:w-8 sm:h-8"/>
+               <img src="/logo.png" alt=""  className="w-6 h-6 sm:w-8 sm:h-8"/>
               </div>
               <span className="text-lg sm:text-xl font-bold">
                 <span className="text-white">Code</span>
